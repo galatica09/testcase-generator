b2db41f2362d8a05ba473ef726df175f8101462b
galactica09 <foxtestagent1@gmail.com>
2025-03-16 11:29:07 +0530

    Add new feature

--- testcasegenerator/testingapp/views.py
+++ testcasegenerator/testingapp/views.py
@@ -2,3 +2,6 @@ from django.http import JsonResponse
 
 def home(request):
     return JsonResponse({"message": "Hello from app1!"})
+
+def contact(request):
+    return JsonResponse({"message": "Contact us!"})
