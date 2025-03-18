16ab5f850a75b442a88ff8e34a5c51b42ab5a75a
galatica09 <foxtestagent1@gmail.com>
2025-03-18 22:30:42 +0530

    Update test cases for testcasegenerator/testingapp2/views.py

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,36 +1,82 @@
 ```python
 import pytest
-from django.urls import reverse
-from django.test import Client
+from rest_framework import status
+from rest_framework.test import APIClient
+from testingapp2.models import Task
+
+pytestmark = pytest.mark.django_db
+
+@pytest.fixture
+def create_task():
+    def _create_task(description="Test Task", completed=False):
+        task = Task.objects.create(description=description, completed=completed)
+        return task
+    return _create_task
 
 @pytest.fixture
 def api_client():
-    return Client()
+    return APIClient()
 
-def test_home_view(api_client):
-    url = reverse('home')
+def test_task_create(api_client, create_task):
+    url = '/tasks/'
+    data = {'description': 'Test Task from API'}
+    response = api_client.post(url, data, format='json')
+    assert response.status_code == status.HTTP_201_CREATED
+    assert Task.objects.count() == 1
+    assert Task.objects.get().description == data['description']
+
+def test_task_create_missing_data(api_client):
+    url = '/tasks/'
+    data = {}
+    response = api_client.post(url, data, format='json')
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+def test_task_retrieve(api_client, create_task):
+    task = create_task()
+    url = f'/tasks/{task.id}/'
     response = api_client.get(url)
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello from app1!"}
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data['id'] == task.id
+    assert response.data['description'] == task.description
 
-def test_contact_view(api_client):
-    url = reverse('contact')
+def test_task_retrieve_not_found(api_client):
+    url = '/tasks/999/'
     response = api_client.get(url)
-    assert response.status_code == 200
-    assert response.json() == {"message": "Contact us!"}
+    assert response.status_code == status.HTTP_404_NOT_FOUND
 
 
-#Negative scenarios (though there aren't obvious negative scenarios for these simple views, we can test for unexpected methods)
-
-def test_home_view_post_request(api_client):
-    url = reverse('home')
+def test_task_complete(api_client, create_task):
+    task = create_task()
+    url = f'/tasks/{task.id}/complete/'
     response = api_client.post(url)
-    assert response.status_code == 405 # Or another appropriate status code indicating method not allowed
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data['status'] == 'Task marked as completed'
+    task.refresh_from_db()
+    assert task.completed is True
 
-
-def test_contact_view_post_request(api_client):
-    url = reverse('contact')
+def test_task_complete_not_found(api_client):
+    url = '/tasks/999/complete/'
     response = api_client.post(url)
-    assert response.status_code == 405 # Or another appropriate status code indicating method not allowed
+    assert response.status_code == status.HTTP_404_NOT_FOUND
 
+def test_task_update(api_client, create_task):
+    task = create_task()
+    url = f'/tasks/{task.id}/'
+    new_description = "Updated Task Description"
+    response = api_client.patch(url, {'description': new_description}, format='json')
+    assert response.status_code == status.HTTP_200_OK
+    task.refresh_from_db()
+    assert task.description == new_description
+
+def test_task_delete(api_client, create_task):
+    task = create_task()
+    url = f'/tasks/{task.id}/'
+    response = api_client.delete(url)
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert Task.objects.count() == 0
+
+def test_task_delete_not_found(api_client):
+    url = '/tasks/999/'
+    response = api_client.delete(url)
+    assert response.status_code == status.HTTP_404_NOT_FOUND
 ```
