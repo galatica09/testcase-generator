b830593975e7b3bae109559791a1469b6e05a869
galactica09 <foxtestagent1@gmail.com>
2025-03-18 22:00:51 +0530

    initial-commit

--- testcasegenerator/testingapp2/views.py
+++ testcasegenerator/testingapp2/views.py
@@ -1,4 +1,17 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import action
+from .serializers import TaskSerializer
+from .models import Task
 
-def about(request):
-    return JsonResponse({"message": "Hello from app2!"})
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    @action(detail=True, methods=['post'])
+    def complete(self, request, pk=None):
+        task = get_object_or_404(Task, pk=pk)
+        task.completed = True
+        task.save()
+        return Response({'status': 'Task marked as completed'})
